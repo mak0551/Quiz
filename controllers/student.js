d2e6792a3136c6e5e55e17d9c8571dd7defd2a2e
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2024-12-27 19:21:23 +0530

    get and update apis for student

--- controllers/student.js
+++ controllers/student.js
@@ -14,3 +14,50 @@ export const createStudent = async (req, res) => {
       .json({ eror: "internal server error", message: err.message });
   }
 };
+
+export const getAllStudents = async (req, res) => {
+  try {
+    const students = await pool.query("select * from student");
+    res.status(200).json(students.rows);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ eror: "internal server error", message: err.message });
+  }
+};
+
+export const getSingleStudent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const students = await pool.query(
+      "select * from student where student_id = $1",
+      [id]
+    );
+    res.status(200).json(students.rows);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ eror: "internal server error", message: err.message });
+  }
+};
+
+export const updateStudent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { user_id, school_id } = req.body;
+    const updatedStudent = await pool.query(
+      "update student set user_id= $1, school_id= $2 where student_id= $3 RETURNING *",
+      [user_id, school_id, id]
+    );
+    if (updatedStudent.rows.length === 0) {
+      return res.status(404).json({ error: "no records found" });
+    }
+    res.status(200).json(updatedStudent.rows[0]);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "internal server error", message: err.message });
+  }
+};
+
+export const deleteStudent = async (req, res) => {};
--- routes/students.js
+++ routes/students.js
@@ -1,7 +1,18 @@
 import express from "express";
-import { createStudent } from "../controllers/student.js";
+import {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+  updateStudent,
+} from "../controllers/student.js";
 const router = express.Router();
 
 router.post("/create", createStudent);
+
+router.get("/getall", getAllStudents);
+
+router.get("/getbyid/:id", getSingleStudent);
+
+router.put("/update/:id", updateStudent);
 
 export default router;
